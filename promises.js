1a5f640e73492580b0c94b4c766f34cb765bb731
Ruben G <rubenngalvann99@gmail.com>
2024-05-22 18:11:02 -0500

    working on pairedprogramming

--- promises.js
+++ promises.js
@@ -1,16 +1,16 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
 
-function num1() {
-    return 1
-}
+// function num1() {
+//     return 1
+// }
 
 async function num2() {
     return 2;
 }
 
-console.log('num1', num1());
-console.log('num2', num2().then(result => console.log(result)));
+// console.log('num1', num1());
+// console.log('num2', num2().then(result => console.log(result)));
 
 
 
@@ -29,39 +29,50 @@ waiting();
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
-// Your code here
+async function waitForMyPromise() {
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('done!!!');
+        }, 1000)
+    });
+
+    const result = await promise;
+    console.log('my promise is', result);
+}
+
+waitForMyPromise();
 
 
 
 /* ============================== Phase 4 ============================== */
 /* -------------------------- exploring then --------------------------- */
 
-// Your code here 
+// Your code here
 
 
 
 /* ============================== Phase 5 ============================== */
 /* ------------------- turn setTimeout into a Promise ------------------ */
 
-// Your code here 
+// Your code here
 
 
 
 /* ============================== Phase 6 ============================== */
 /* -------------------- exploring reject and .catch -------------------- */
 
-// Your code here 
+// Your code here
 
 
 
 /* ============================== Phase 7 ============================== */
 /* ---------------- exploring async/await and try/catch ---------------- */
 
-// Your code here 
+// Your code here
 
 
 
 /* ============================== Phase 8 ============================== */
 /* -------------------- Promises are asynchronous! --------------------- */
 
-// Your code here 
+// Your code here
