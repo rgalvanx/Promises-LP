5f13020eb8ba166e2607022bcf52e8e6e13bf282
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-05-22 17:23:54 -0500

    phase 2 done

--- promises.js
+++ promises.js
@@ -1,21 +1,35 @@
 /* ============================== Phase 1 ============================== */
 /* -------------------------- exploring async -------------------------- */
 
-// Your code here 
+function num1() {
+    return 1
+}
+
+async function num2() {
+    return 2;
+}
+
+console.log('num1', num1());
+console.log('num2', num2().then(result => console.log(result)));
 
 
 
 /* ============================== Phase 2 ============================== */
 /* -------------------------- exploring await -------------------------- */
 
-// Your code here 
+async function waiting() {
+    const value = await num2();
+    console.log('waiting', value)
+}
+
+waiting();
 
 
 
 /* ============================== Phase 3 ============================== */
 /* --------------------- creating a custom Promise --------------------- */
 
-// Your code here 
+// Your code here
 
 
 
